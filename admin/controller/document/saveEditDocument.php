16a9dc8dbc20fd47aace29b6f76310852c003b40
daocutam <leejongsuk189@gmail.com>
2019-08-15 22:28:17 +0700

    no message

--- admin/controller/document/saveEditDocument.php
+++ admin/controller/document/saveEditDocument.php
@@ -1,13 +1,19 @@
 <?php
 include('../../configDb.php');
-
-if (isset($_POST['id']) && isset($_POST['name']) && isset($_POST['branch_id']) && isset($_POST['link']) && isset($_POST['image'])) {
-    $id = (int) $_POST['id'];
-    $branch_id = (int) $_POST['branch_id'];
-    $name = $_POST['name'];
-    $link = $_POST['link'];
-    $str_img = substr($_POST['image'], 12);
-    $image = "images/" . $str_img;
+if (isset($_FILES['file'])) {
+    $file_name = $_FILES['file']['name'];
+    $file_type = $_FILES['file']['type'];
+    $file_size = $_FILES['file']['size'];
+    $file_temp_loc = $_FILES['file']['tmp_name'];
+    $file_store = "../../../admin/Images/" . $file_name;
+    move_uploaded_file($file_temp_loc, $file_store);
+}
+if (isset($_POST['_id']) && isset($_POST['_name']) && isset($_POST['_branch_id']) && isset($_POST['_link'])) {
+    $id = (int) $_POST['_id'];
+    $branch_id = (int) $_POST['_branch_id'];
+    $name = $_POST['_name'];
+    $link = $_POST['_link'];
+    $image =  $file_name;
     $sql = "UPDATE document SET  branch_id='$branch_id',name='$name',link='$link',image='$image' WHERE id='$id'";
     if ($conn->query($sql) == TRUE) {
         echo 1;
