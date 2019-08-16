221f81f96b351c9978c2e1be6fb34be13cfd1fad
thuypn52 <thuypn52@wru.vn>
2019-08-16 14:39:13 +0700

    no message

---
+++ dhtl/hop_tac_lien_ket.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Thư viện css -->
+	<link rel="stylesheet" href="../admin/public/templates/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="../admin/public/templates/css/style.css" />
+	<link rel="stylesheet" href="../admin/public/templates/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="../admin/public/templates/css/aos.css" />
+	<!-- Thư viện js -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="../admin/public/templates/js/bootstrap.min.js"></script>
+	<script src="../admin/public/templates/js/aos.js"></script>
+	<title>Hợp tác liên kết</title>
+</head>
+
+<body>
+	<!-- Header top -->
+	<?php include('top_header.php'); ?>
+	<!-- End Header top -->
+
+	<!-- Header_Menu -->
+	<?php include('menu_header.php'); ?>
+	<!-- End Header_Menu -->
+
+	<!-- Carousel -->
+	<?php include('slider.php'); ?>
+	<!-- End Carousel -->
+	
+	<!-- MAIN -->
+
+	<!-- End Main  -->
+		<section class="container main_logo">
+		<section class="row">
+			<section class="menu_main_logo">
+				<a href="">Giới Thiệu</a>
+				<img src="http://cse.tlu.edu.vn/Portals/_default/Skins/Xcillion/Images/breadcrumb-arrow.png" alt="">
+				<a href="">Hợp tác liên kết</a>
+			</section>
+		</section>
+	</section>
+	<!-- Footer -->
+	<?php include('footer.php'); ?>
+	<!-- End Footer -->
+</body>
+
+</html>
